2846e8a4635a17a0ca8b5b0a20df867fe9e4ae69
Anubhav Bhutani <anubhavbhutani@Anubhavs-MacBook-Air.local>
2024-10-07 16:23:19 +0530

    Refined the code for RegisterPage

--- frontend/src/Pages/RegisterPage.js
+++ frontend/src/Pages/RegisterPage.js
@@ -1,62 +1,36 @@
 import './LoginRegister.css';
 import Header from '../components/Header';
 import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios'
 import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer, toast } from 'react-toastify';
 import validator from 'validator'
+import { json } from 'react-router-dom';
 
 // Register Page.
 
 export default function RegisterPage() {
-  // The below message state will be managed by the useState functionality.
+
+  // Use States
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [email,setEmail] = useState('')
   const [message,setMessage] = useState('')
-  
-  // The function that will be responsible for submitting the form.
+
   async function register(ev) {
     ev.preventDefault();
-
-    // Conditions for successful register
-    if(!validator.isEmail(email)) {
-      setMessage('Enter a valid Email Address.')
-      return
-    }
-    if(username.length == 0) {
-        setMessage('Username cannot be empty!')
-        return
-    }
-    if(!validator.isStrongPassword(password)) {
-      setMessage(`Set a  Stronger Password \n
-        The password Should Contain: \n
-        1. Uppercase (A-Z) \n
-        2. Lowercase (a-z) \n
-        3. Numbers (0-9) \n
-        4. Should be greater than 8 Characters. \n
-        `)
-      return
-    }
     
-    
-    // The program will wait for this function to complete execution, then 
-    // only it will be going to the next line.
-
-    const res = await fetch('http://localhost:4000/register', {
-      method: 'POST', // Should likely be POST for registration, not GET
-      body: JSON.stringify({ email,username, password }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    // Depends if the response status is 200 or not.
-    if(res.status == 200) {
-      setMessage('Registered User Successfully. Redirecting to Login Page.')
-      setTimeout(function() {
-        location.assign('/login')
-      },3000)
+    try{
+      const res = await axios.post('http://localhost:4000/register', {
+        email,
+        username,
+        password
+      });
+      setMessage(res.data.message)
     }
-    else if(res.status === 401) {
-      setMessage('User Already Exists. Kindly login!')
+    catch(error) {
+      console.log(`Axios Error: ${error}`);
     }
   }
 
@@ -66,13 +40,12 @@ export default function RegisterPage() {
       <h1>Register</h1>
       <form className="register" onSubmit={register}>
       <input
-          type="string"
+          type="email"
           value={email}
           onChange={(ev) => 
             {
               setEmail(ev.target.value)
             }} 
-            // Corrected
             placeholder="Email"
             />
         <input
