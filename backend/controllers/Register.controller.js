d8f4d859f79d1bd7c51c81799f804ab06baf1a34
Anubhav Bhutani <anubhavbhutani@Anubhavs-MacBook-Air.local>
2024-10-07 15:19:34 +0530

    Redefined the Register Controller

--- backend/controllers/Register.controller.js
+++ backend/controllers/Register.controller.js
@@ -1,36 +1,16 @@
-const User = require('../models/User.model')
+const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+const env = require("dotenv").config();
 
-async function RegisterUser(req,res) {
-    const {email,username,password} = req.body
-    console.log(email + " " + username + ' ' + password);
-
-    try {
-    const user = await User.findOne({username})
-    const user2 = await User.findOne({username})
-
-    if(user || user2) {
-        res.sendStatus(401)
-        return
-    }
-    }
-catch(error) 
-{
-    res.sendStatus(401)
-    return
+async function RegisterUser(req, res) {
+  try {
+    const { email, username, password } = req.body;
+    const newUser = new User({ email, username, password });
+    await newUser.save()
+    return res.send({message:'Registered User Sucessfully!',status:200})
+  } catch (error) {
+    return res.send({ message: "User Already Registered!",error:error.message,errorCode:error.code,status:500});
+  }
 }
 
-    try {
-        // const newUser = User.createUser({email,username,password})
-        const newUser = new User({email,username,password})
-        const userSaved = await newUser.save()
-        console.log(`User Created Successfully.`)
-        res.sendStatus(200)
-    }
-    catch(e) {
-        console.log(e)
-        res.sendStatus(400)
-    }
-    
-}
-
-module.exports = RegisterUser
+module.exports = RegisterUser;
