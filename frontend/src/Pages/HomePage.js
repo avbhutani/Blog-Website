c4db4e6076c26d4933ee897ff734443dc8aa7344
avbhutani <avbhutani3@gmail.com>
2024-10-13 13:08:18 +0530

    Added Get All Posts Feature and Also Made the Post Component, Also rendered it on the homepage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -4,6 +4,7 @@ import CheckAccess from "../utils/CheckAccess";
 import GetUser from '../utils/GetUser';
 import { useNavigate } from 'react-router-dom';
 import Post from '../components/Post';
+import PostsList from '../components/Posts';
 export default function HomePage(props) {
     const user = CheckAccess('/login')
     
@@ -11,7 +12,7 @@ export default function HomePage(props) {
         <>
         <Header />
         {user?<h1>Welcome Home, {user.username}</h1>:<h1>Unauthorized</h1>}
-        <Post />
+        <PostsList />
         </>
     )
 }
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,27 +1,23 @@
 // Component for Post. V
 
-// The content can be dynamic, just the model structure would remain the same. 
+// The content can be dynamic, just the model structure would remain the same.
 // Rest all the data can be replaced, with the data from the backend.
-import CheckAccess from '../utils/CheckAccess'
-import './Post.css'
-export default function Post(){
-  const user = CheckAccess(null)
-    return(
-        <div className='post'>
-          <div className='post-left'>
-          <h2 className='post-name'>
-            Post Image
-          </h2>
-          </div>
-          <div className='post-right'>
-          <h3 className='post-content'>
-            Post Content
-          </h3>
-          <p className='post-comments'>
-            Post Comments
-          </p>
-          </div>
-          {/* {user? <button>Edit</button>: <h1>Unauthorized</h1>} */}
+import CheckAccess from "../utils/CheckAccess";
+import "./Post.css";
+export default function Post({post}) {
+  const user = CheckAccess(null);
+  return (
+    <>
+      <div className="post">
+        <div className="post-img">
+          <img src={post.img} alt="Img" />
         </div>
-    )
+        <div className="post-contents">
+          <h2 className="post-title">{post.title}</h2>
+          <h3 className="post-author">~{post.author}</h3>
+          <p className="post-content">{post.content}</p>
+        </div>
+      </div>
+    </>
+  );
 }
